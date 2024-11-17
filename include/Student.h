5ab421418dcee2012066b4faf40762cdfaca624d
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-11-17 20:26:29 +0200

    visuale use of autogenerated copy constructor

--- include/Student.h
+++ include/Student.h
@@ -7,25 +7,12 @@
 class Student {
 private:
     int id;
-    char* name;
     float grade;
 
 public:
+    char* name;
     Student(int studentId, const char* studentName, float studentGrade);
-
     ~Student();
-
-    Student(const Student& other);
-
-    Student(Student&& other) noexcept;
-
-    int getId() const;
-    const char* getName() const;
-    float getGrade() const;
-
-    void setId(int newId);
-    void setName(const char* newName);
-    void setGrade(float newGrade);
 
     void display() const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include "Student.h"
+#include <iostream>
 
 int main() {
+    // create a student
     Student student1(1, "Vlad Ciobotariu", 9.2);
     student1.display();
 
+    // copy constrcutor, auto generated
     Student student2 = student1;
     student2.display();
 
-    Student student3 = std::move(student1);
-    student3.display();
-
+    // modify
+    std::cout << "modify..." << std::endl;
+    delete[] student2.name;
     student1.display();
-
-    student2.setName("Marcel Ciolacu");
-    student2.setGrade(8.7);
-    student2.display();
 
     return 0;
 }
